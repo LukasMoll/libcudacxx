55aaac465f54f18a0f59aa9ec85458f1613a5254
LukasMol <lukas.molleman@ugent.be>
2022-08-03 21:18:37 +0200

    libunwind/ made python files pythonic

--- libunwind/test/libunwind/test/config.py
+++ libunwind/test/libunwind/test/config.py
@@ -1,12 +1,11 @@
-#===----------------------------------------------------------------------===##
+# ===----------------------------------------------------------------------===##
 #
 # Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #
-#===----------------------------------------------------------------------===##
+# ===----------------------------------------------------------------------===##
 import os
-import sys
 
 from libcxx.test.config import Configuration as LibcxxConfiguration
 
@@ -22,16 +21,16 @@ class Configuration(LibcxxConfiguration):
 
     def configure_src_root(self):
         self.libunwind_src_root = (self.get_lit_conf('libunwind_src_root')
-            or os.path.dirname(self.config.test_source_root))
+                                   or os.path.dirname(self.config.test_source_root))
         self.libcxx_src_root = (self.get_lit_conf('libcxx_src_root')
-            or os.path.join(self.libunwind_src_root, '..', 'libcxx'))
+                                or os.path.join(self.libunwind_src_root, '..', 'libcxx'))
 
     def configure_obj_root(self):
         self.libunwind_obj_root = self.get_lit_conf('libunwind_obj_root')
         super(Configuration, self).configure_obj_root()
 
     def has_cpp_feature(self, feature, required_value):
-        return int(self.cxx.dumpMacros().get('__cpp_' + feature, 0)) >= required_value
+        return int(self.cxx.dump_macros().get('__cpp_' + feature, 0)) >= required_value
 
     def configure_features(self):
         super(Configuration, self).configure_features()
